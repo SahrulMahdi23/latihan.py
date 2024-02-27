720e1a430187bb33a130b1423876d0450eb22ad4
SahrulMahdi23 <147253671+SahrulMahdi23@users.noreply.github.com>
2024-02-27 22:36:21 +0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+print("=== aplikasi biodata ===")
+name =  input("masukan nama :")
+gender =  input("masukan gender :")
+tanggal_Lahir = input("masukan tanggal lahir :")
+hobi = input("masukan hobi :")
+
+print("nama :", name)
+print("gender :", gender)
+print("tanggal lahir :",tanggal_Lahir)
+print("hobi :", hobi)
